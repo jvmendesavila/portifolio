4e56a31ca7cd5c0955b1c4aee91de6919dcef752
JOAO_VICTOR <joaovictormendesavila@gmail.com>
2023-03-12 11:08:58 -0300

    fix(seo): change image path from meta tags home page

--- src/app/[lang]/head.tsx
+++ src/app/[lang]/head.tsx
@@ -7,16 +7,21 @@ export default function Head() {
         name="description"
         content="João Victor Mendes Ávila | Desenvolvedor de Software | Front-end Pleno"
       />
-      <link rel="image" href="/images/pages/meta-index.png" />
+      <link
+        rel="image"
+        href="https://portifolio-jvmendesavila.vercel.app/images/pages/meta-index.png"
+      />
 
       {/* OG */}
-
       <meta property="og:title" content="Olá! 👋 Bem vindo ao meu potifólio" />
       <meta
         property="og:description"
         content="João Victor Mendes Ávila | Desenvolvedor de Software | Front-end Pleno"
       />
-      <meta property="og:image" content="/images/pages/meta-index.png" />
+      <meta
+        property="og:image"
+        content="https://portifolio-jvmendesavila.vercel.app/images/pages/meta-index.png"
+      />
     </>
   );
 }
